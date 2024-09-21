5332a365ad207a160da849bb8699319838bee649
SorelliGamer <162809184+SorelliGamer@users.noreply.github.com>
2024-09-21 11:55:56 -0300

    AUla 4

--- AULA 4/index.php
+++ AULA 4/index.php
@@ -1,15 +1,27 @@
+<?php
+    session_start();
+
+    if(isset($_GET['ok'])){
+        $itens = $_SESSION['itens'];
+    }
+    else {
+        $itens =[];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main class="container">
         <h1>Top Vendas</h1>
-        <form  action="verificar.php" method="post" class="container-input">
-            <input type="text" nome="pesquisa" id="pesquisa">
+        <form  action="verificar.php" method="POST" class="container-input">
+            <input type="text" name="pesquisa" id="pesquisa">
             <button type="submit">🔎</button>
         </form>
         <table>
@@ -19,8 +31,20 @@
                 <th>Marca</th>
                 <th>Quantidade</th>
             </tr>
-            <!-- CODIGO PHP -->
-
+            <?php if (!empty($itens)): ?>
+                <?php foreach ($itens as $item): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($item[0]); ?></td>
+                        <td><?php echo htmlspecialchars($item[1]); ?></td>
+                        <td><?php echo htmlspecialchars($item[2]); ?></td>
+                        <td><?php echo htmlspecialchars($item[3]); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspar="4" style="text-align:cemter;">Nenhum Item encontrado.</td>
+                </tr>
+            <?php endif; ?> 
         </table>
     </main>
 </body>
