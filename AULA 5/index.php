b09e3c931d002540755fe0c20ddf3c04e387b6e6
SorelliGamer <162809184+SorelliGamer@users.noreply.github.com>
2024-09-28 12:00:54 -0300

    Aula 5

---
+++ AULA 5/index.php
@@ -0,0 +1,44 @@
+<?php
+include('../conexoes/conexao_estoque.php');
+
+$sql_codigo = 'SELECT * FROM itens';
+$resultado = $mysqli->query($sql_codigo);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pada Watson</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>
+        Pada Watson
+    </h1>
+    <main class="container">
+        <table>
+            <tr class="cabecalho">
+                <th>Nome</th>
+                <th>Quantidade</th>
+                <th>Ações</th>
+            </tr>
+            <?php
+                if ($resultado->num_rows >= 1){
+                    While($item = $resultado->fetch_assoc()){
+                        echo '<tr class="infos">';
+                            echo '<td>'.$item['Nome'].'</td>';
+                            echo '<td>'.$item['Quantidade'].'</td>';
+                            echo '<td><a href="editar.php?id='.$item['Id'].'">editar</a></td>';
+                            echo '<td>deletar</td>';
+                    echo '</tr>';   
+                    }               
+                }
+            ?>
+        </table>
+    </main>
+</body>
+</html>
